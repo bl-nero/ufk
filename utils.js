20a42e4baa24d3a19fe5524d0195e7de913658fc
Bartosz Leper <bleper@google.com>
2015-01-29 10:08:26 +0100

    utils

---
+++ utils.js
@@ -0,0 +1,5 @@
+var viewport = document.createElement("meta");
+viewport.setAttribute('name', 'viewport');
+viewport.setAttribute('content', 'width=device-width, initial-scale=1');
+document.getElementsByTagName('head')[0].appendChild(viewport);
+
